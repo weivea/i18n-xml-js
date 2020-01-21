7c65aa78612f2be135c49cd85753654055eaab97
weijianli <weijianli@xiaomi.com>
2020-01-21 18:45:39 +0800

    react-example

---
+++ docs/react/precache-manifest.168edf53dd651ed9a2ec5661587b1d52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6613282ab5d3bf9816ac70661f6df97e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8daf82d4d3b13be3ba8f",
+    "url": "./static/css/main.07b4fbce.chunk.css"
+  },
+  {
+    "revision": "6f38ac080aba64f4fd16",
+    "url": "./static/js/2.def4a2de.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.def4a2de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "391e076569e22e549097",
+    "url": "./static/js/3.1f550beb.chunk.js"
+  },
+  {
+    "revision": "8daf82d4d3b13be3ba8f",
+    "url": "./static/js/main.2d937482.chunk.js"
+  },
+  {
+    "revision": "e04f5fd0199c58e15aca",
+    "url": "./static/js/runtime-main.9c63626d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
